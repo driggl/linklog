1cd9bad3b8067046db16c64f177ef33b1f787449
Sebastian Wilgosz <sebastian@driggl.com>
2020-05-15 14:20:05 +0200

    Add factory bot gem for rspec configuration

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'jsonapi_errors_handler', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
   gem 'rspec-rails', '>=4.0.0'
 end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include FactoryBot::Syntax::Methods
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
