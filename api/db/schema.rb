018fab3be322c371668e7f4c35131e98f66a5c53
Sebastian Wilgosz <sebastian@driggl.com>
2020-05-28 10:18:15 +0200

    Add excerpt to the articles

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_105158) do
+ActiveRecord::Schema.define(version: 2020_05_27_195413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_105158) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "parsed_content"
+    t.text "excerpt"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
