266970c2937a0fc1bd260cb548d4fbf7fb5e8a49
lukasz <lukasz.usarz@gmail.com>
2020-05-28 08:05:39 +0200

    Article details

---
+++ ui/test/specs/pages/article-details.spec.js
@@ -0,0 +1,33 @@
+import { mount, buildStore } from '~/test/utils'
+import ArticleDetailsPage from '@/pages/articles/_slug/index.vue'
+
+describe('ArticleDetailsPage', () => {
+  let wrapper
+  function mountComponent() {
+    wrapper = mount(ArticleDetailsPage, {
+      mocks: {
+        $route: {
+          params: { slug: 'Sample article title' }
+        }
+      },
+      store: buildStore('articles', {
+        getters: {
+          findArticle: () => () => ({
+            title: 'Sample article title',
+            content: '# This is title'
+          })
+        }
+      })
+    })
+  }
+
+  test('article title is displayed', () => {
+    mountComponent()
+    expect(wrapper.find('.container h1').text()).toEqual('Sample article title')
+  })
+
+  test('article content is displayed', () => {
+    mountComponent()
+    expect(wrapper.find('#article-content').text()).toEqual('This is title')
+  })
+})
