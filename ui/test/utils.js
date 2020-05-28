82200225c53aa082ff91b4a07114bb9e22c40fbe
Łukasz Usarz <lukasz.usarz@gmail.com>
2020-05-28 07:51:26 +0200

    Unit tests configuration (#22)

---
+++ ui/test/utils.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+import * as VueTestUtils from '@vue/test-utils'
+
+Vue.use(Vuetify)
+Vue.use(Vuex)
+
+export function shallow(Component, config) {
+  const localVue = VueTestUtils.createLocalVue()
+  localVue.use(Vuetify)
+  localVue.use(Vuex)
+
+  return VueTestUtils.shallowMount(Component, {
+    vuetify: new Vuetify({ theme: {} }),
+    stubs: ['Nuxt', 'router-link'],
+    localVue,
+    ...config
+  })
+}
+
+export function mount(Component, config) {
+  const localVue = VueTestUtils.createLocalVue()
+  localVue.use(Vuetify)
+  localVue.use(Vuex)
+
+  return VueTestUtils.mount(Component, {
+    vuetify: new Vuetify({ theme: {} }),
+    stubs: ['Nuxt', 'router-link'],
+    localVue,
+    ...config
+  })
+}
+
+export function buildStore(name, config) {
+  return buildStores([{ name, ...config }])
+}
+
+export function buildStores(stores) {
+  const modules = {}
+  stores.forEach((config) => {
+    modules[config.name] = {
+      namespaced: true,
+      state: config.state || {},
+      getters: config.getters || {},
+      actions: config.actions || {},
+      mutations: config.mutations || {}
+    }
+  })
+
+  return new Vuex.Store({ modules })
+}
