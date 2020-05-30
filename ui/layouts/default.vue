30f0bbc68f0ea382308286994b058da1450da883
Sebastian Wilgosz <sebastian@driggl.com>
2020-05-30 02:57:53 +0200

    Test commit

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -12,6 +12,7 @@
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
+
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
