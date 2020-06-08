cf7d710a25a7272df2b1b918f177ae5897f84a35
lukasz <lukasz.usarz@gmail.com>
2020-06-08 14:53:29 +0200

    UI: Submit form on enter, article form validation

--- ui/components/ArticleForm.vue
+++ ui/components/ArticleForm.vue
@@ -1,28 +1,46 @@
 <template>
-  <v-form>
+  <v-form v-model="valid" @submit.prevent="$emit('save')">
     <div>
-      <v-text-field v-model="article.title" label="Title" />
+      <v-text-field
+        v-model="article.title"
+        label="Title"
+        :rules="[rules.required]"
+      />
     </div>
     <div>
       <v-textarea
         v-model="article.content"
         label="Content"
+        :rules="[rules.required, rules.minLength5]"
         auto-grow
         :rows="1"
       />
     </div>
     <div class="text-right">
-      <v-btn @click="$emit('save')">Save</v-btn>
+      <v-btn :disabled="!valid" type="submit">Save</v-btn>
     </div>
   </v-form>
 </template>
 
 <script>
+import * as ValidationUtils from '~/lib/utils/validation-utils'
+
 export default {
   props: {
     article: {
       type: Object,
       default: null
+    }
+  },
+  data() {
+    return {
+      rules: {
+        required: (v) =>
+          ValidationUtils.isNotEmpty(v) || 'This field is required',
+        minLength5: (v) =>
+          v.length > 5 || 'This field need to have at least 5 characters'
+      },
+      valid: false
     }
   }
 }
