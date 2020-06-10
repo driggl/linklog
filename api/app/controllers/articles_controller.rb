7dec33939444e3eb7fd565aa0fe295a443333e14
Sebastian Wilgosz <sebastian@driggl.com>
2020-06-10 21:15:36 +0200

    Add ability to fetch articles by slug

--- api/app/controllers/articles_controller.rb
+++ api/app/controllers/articles_controller.rb
@@ -9,7 +9,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
+    article =
+      Article.find_by(slug: params[:id]) ||
+      Article.find(params[:id])
     render json: serializer.new(article, include: included)
   end
 
