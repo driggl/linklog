a20ffb5bac819b07bd8c23b9b034703edc23fa5c
lusarz <lukasz.usarz@gmail.com>
2020-06-22 19:25:38 +0200

    UI: Add policy privacy checkbox into registration form

--- ui/lib/utils/validation-utils.js
+++ ui/lib/utils/validation-utils.js
@@ -7,5 +7,5 @@ export function isNotEmpty(value) {
   if (Array.isArray(value)) {
     return value.length > 0
   }
-  return value != null && value !== ''
+  return value !== false && value != null && value !== ''
 }
