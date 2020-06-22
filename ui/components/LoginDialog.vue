a517f9d05db65b429022fb3274a8e58330a9f2e8
lusarz <lukasz.usarz@gmail.com>
2020-06-22 16:31:31 +0200

    UI: Move common dialog form logic into single

--- ui/components/LoginDialog.vue
+++ ui/components/LoginDialog.vue
@@ -1,44 +1,30 @@
 <template>
-  <div>
-    <v-dialog width="500" :value="visible" @input="$emit('update:visible', false)">
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>
-          Login
-        </v-card-title>
-
-        <v-form v-model="valid" @submit.prevent="login">
-          <v-card-text class="pt-4">
-            <div v-if="errorMessage" class="error-message">
-              {{ errorMessage }}
-            </div>
-            <v-text-field id="login-input" v-model="form.login" :rules="[rules.required]" label="Login" />
-            <v-text-field
-              id="password-input"
-              v-model="form.password"
-              :rules="[rules.required, rules.minLength]"
-              label="Password"
-              type="password"
-            />
-          </v-card-text>
-
-          <v-divider />
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" :disabled="!valid" type="submit">
-              Log in
-            </v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-dialog>
-  </div>
+  <base-dialog-form
+    :visible="visible"
+    title="Login"
+    submit-title="Log in"
+    :submit="login"
+    @hide="$emit('update:visible', false)"
+  >
+    <v-text-field id="login-input" v-model="form.login" :rules="[rules.required]" label="Login" />
+    <v-text-field
+      id="password-input"
+      v-model="form.password"
+      :rules="[rules.required, rules.minLength]"
+      label="Password"
+      type="password"
+    />
+  </base-dialog-form>
 </template>
 
 <script>
+import BaseDialogForm from '~/components/base/BaseDialogForm'
 import * as ValidationUtils from '~/lib/utils/validation-utils'
 
 export default {
+  components: {
+    BaseDialogForm
+  },
   props: {
     visible: {
       type: Boolean,
@@ -51,38 +37,27 @@ export default {
         login: '',
         password: ''
       },
-      valid: false,
       rules: {
         required: (v) => ValidationUtils.isNotEmpty(v) || 'This field is required',
         minLength: (v) => v.length > 4 || 'This field need to have at least 5 characters'
-      },
-      errorMessage: null
+      }
+    }
+  },
+  watch: {
+    visible() {
+      Object.assign(this.form, { login: '', password: '' })
     }
   },
 
   methods: {
     async login() {
-      try {
-        const data = {
-          data: {
-            attributes: this.form
-          }
+      const data = {
+        data: {
+          attributes: this.form
         }
-        await this.$auth.loginWith('local', { data })
-        this.$emit('update:visible', false)
-      } catch (e) {
-        const error = e.response.data.errors[0]
-        this.errorMessage = `${error.title} - ${error.detail}`
       }
+      await this.$auth.loginWith('local', { data })
     }
   }
 }
 </script>
-
-<style scoped>
-.error-message {
-  color: red; /* TODO: use variable */
-  margin-top: 14px;
-  margin-bottom: 14px;
-}
-</style>
--- ui/components/RegistrationDialog.vue
+++ ui/components/RegistrationDialog.vue
@@ -1,53 +1,39 @@
 <template>
-  <div>
-    <v-dialog width="500" :value="visible" @input="$emit('update:visible', false)">
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>
-          Registration
-        </v-card-title>
-
-        <v-form v-model="valid" @submit.prevent="register">
-          <v-card-text class="pt-4">
-            <div v-if="errorMessage" class="error-message">
-              {{ errorMessage }}
-            </div>
-            <v-text-field id="name-input" v-model="form.name" :rules="[rules.required]" label="Name" type="text" />
-            <v-text-field
-              id="email-input"
-              v-model="form.email"
-              :rules="[rules.required, rules.email]"
-              label="Email"
-              type="email"
-            />
-            <v-text-field id="login-input" v-model="form.login" :rules="[rules.required]" label="Login" />
-            <v-text-field
-              id="password-input"
-              v-model="form.password"
-              :rules="[rules.required, rules.minLength]"
-              label="Password"
-              type="password"
-            />
-          </v-card-text>
-
-          <v-divider />
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" :disabled="!valid" type="submit">
-              Register
-            </v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-dialog>
-  </div>
+  <base-dialog-form
+    :visible="visible"
+    title="Registration"
+    submit-title="Register"
+    :submit="register"
+    @hide="$emit('update:visible', false)"
+  >
+    <v-text-field id="name-input" v-model="form.name" :rules="[rules.required]" label="Name" type="text" />
+    <v-text-field
+      id="email-input"
+      v-model="form.email"
+      :rules="[rules.required, rules.email]"
+      label="Email"
+      type="email"
+    />
+    <v-text-field id="login-input" v-model="form.login" :rules="[rules.required]" label="Login" />
+    <v-text-field
+      id="password-input"
+      v-model="form.password"
+      :rules="[rules.required, rules.minLength]"
+      label="Password"
+      type="password"
+    />
+  </base-dialog-form>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
 import * as ValidationUtils from '~/lib/utils/validation-utils'
+import BaseDialogForm from '~/components/base/BaseDialogForm'
 
 export default {
+  components: {
+    BaseDialogForm
+  },
   props: {
     visible: {
       type: Boolean,
@@ -62,35 +48,24 @@ export default {
         login: '',
         password: ''
       },
-      valid: false,
       rules: {
         email: (v) => ValidationUtils.isValidEmail(v) || 'You need to type valid email address',
         required: (v) => ValidationUtils.isNotEmpty(v) || 'This field is required',
         minLength: (v) => v.length > 4 || 'This field need to have at least 5 characters'
-      },
-      errorMessage: null
+      }
+    }
+  },
+  watch: {
+    visible() {
+      Object.assign(this.form, { name: '', email: '', login: '', password: '' })
     }
   },
 
   methods: {
     ...mapActions('user', ['REGISTER']),
     async register() {
-      try {
-        await this.REGISTER(this.form)
-        this.$emit('update:visible', false)
-      } catch (e) {
-        const error = e.response.data.errors[0]
-        this.errorMessage = `${error.title} - ${error.detail}`
-      }
+      await this.REGISTER(this.form)
     }
   }
 }
 </script>
-
-<style scoped>
-.error-message {
-  color: red; /* TODO: use variable */
-  margin-top: 14px;
-  margin-bottom: 14px;
-}
-</style>
