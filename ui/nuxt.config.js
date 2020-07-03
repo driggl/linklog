a99f811dfdfe2d345936bff66cf8c16c4e668be8
Sebastian Wilgosz <sebastian@driggl.com>
2020-07-03 17:45:17 +0200

    Restore old API host in configuration

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -64,7 +64,7 @@ export default {
   },
   proxy: {
     '/api': {
-      target: process.env.API_URL || 'https://webdevflow.herokuapp.com',
+      target: process.env.API_URL || 'https://api.webdevflow.com',
       pathRewrite: {
         '^/api': '/'
       }
