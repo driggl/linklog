17895d1685ab8d9ca0ff22612b61b1d15b76aedc
Sebastian Wilgosz <sebastian@driggl.com>
2020-07-10 00:53:22 +0200

    Upgrade ruby gems

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "api_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
