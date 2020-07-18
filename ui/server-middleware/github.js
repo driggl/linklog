f9de1c335b2fdfd05633ac00f045ad2f9f0c4b20
Sebastian Wilgosz <sebastian@driggl.com>
2020-07-18 17:13:14 +0200

    Fix the redirect after github login

--- ui/server-middleware/github.js
+++ ui/server-middleware/github.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 export default async function(req, res) {
   const code = req._parsedUrl.query.split('&')[0].replace('code=', '')
   try {
-    const response = await axios.post(`https://webdevflow.herokuapp.com/login`, { code })
+    const response = await axios.post(`https://api.webdevflow.com/login`, { code })
     const token = response.data.token
     res.writeHead(308, {
       Location: '/?token=' + token,
