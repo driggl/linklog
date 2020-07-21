9461bd164242c83b1f4d3c833aa20519cb4d0df5
Sebastian Wilgosz <sebastian@driggl.com>
2020-07-21 13:17:21 +0200

    Fix model tests for access token

--- api/spec/models/access_token_spec.rb
+++ api/spec/models/access_token_spec.rb
@@ -17,12 +17,13 @@ RSpec.describe AccessToken, type: :model do
 
   describe '#new' do
     let(:user) { create(:user) }
+
     it 'should have a token present after initialize' do
       expect(AccessToken.new.token).to be_present
     end
 
     it 'should generate uniq token' do
-      expect { user }.to change { AccessToken.count }.by(1)
+      expect { user.create_access_token }.to change { AccessToken.count }.by(1)
       expect(user.build_access_token).to be_valid
     end
 
